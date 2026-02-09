c80d29bff252c09c26328ebf4eff8fb97f03d8a9
parina004 <parinakparekh@gmail.com>
2026-02-09 18:00:50 +0400

    first ver

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import.meta.env.BASE_URL
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+if ("serviceworker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register(
+      `${import.meta.env.BASE_URL}service-worker.js`
+
+    );
+  });
+}
